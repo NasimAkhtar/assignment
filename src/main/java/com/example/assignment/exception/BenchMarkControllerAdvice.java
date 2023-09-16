65f2624c27e8e1776acff28099d763b41e828663
nasimakhtar <nasim@backbase.com>
2023-09-16 23:55:26 +0530

    Added error logger

--- src/main/java/com/example/assignment/exception/BenchMarkControllerAdvice.java
+++ src/main/java/com/example/assignment/exception/BenchMarkControllerAdvice.java
@@ -1,5 +1,7 @@
 package com.example.assignment.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class BenchMarkControllerAdvice {
 
+    private static final Logger logger = LoggerFactory.getLogger(BenchMarkControllerAdvice.class);
+
     @ExceptionHandler(BenchMarkCustomException.class)
     public ResponseEntity<Object> handleException(BenchMarkCustomException exception) {
+        logger.error(exception.getMessage());
         return ResponseEntity.internalServerError().body(exception.getMessage());
     }
 }
